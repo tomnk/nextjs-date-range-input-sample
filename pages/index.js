ae7abd6be215b98b70f21acc06024f55b61b75de
tomnk <tamnk.bap@gmail.com>
2021-09-26 22:53:17 +0700

    add date range picker

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import React, { useState, forwardRef } from 'react';
 import Head from 'next/head';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
@@ -88,16 +89,6 @@ export default function Home() {
           <DateInput />
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-        </a>
-      </footer>
     </div>
   );
 }
